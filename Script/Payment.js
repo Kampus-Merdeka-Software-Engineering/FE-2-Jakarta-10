cfde8c1b36fad60e8885d9a9ec5e25bf76584453
Fawwas17 <fawwsshafiq36@gmail.com>
2023-12-15 23:27:51 +0700

    update

--- Script/Payment.js
+++ Script/Payment.js
@@ -6,7 +6,7 @@ async function processPayment() {
   const bankType = document.getElementById("bankType").value;
 
   try {
-    const response = await fetch('http://localhost:22564/process/payment', {
+    const response = await fetch('https://gray-lively-barnacle.cyclic.app/process/payment', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
